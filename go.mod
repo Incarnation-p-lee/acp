40e3588ba06fd67e1ea4b119c924393471813093
Pan Li <panli@microsoft.com>
2020-04-12 16:31:02 +0800

    Add new for graph.
    Signed-off-by: Pan Li <panli@microsoft.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Incarnation-p-lee/acp
 
 go 1.14
+
+require github.com/stretchr/testify v1.5.1
--- pkg/graph/graph.go
+++ pkg/graph/graph.go
@@ -8,11 +8,23 @@ import (
 // Graph indicates the structure definition for both directed and indirected graph
 type Graph struct {
 	Attributes Attributes
-	Vertexs    map[string]vertex.Vertex
+	Vertexes   map[string]vertex.Vertex
 	Edges      map[string]edge.Edge
 }
 
-// Attributes indicates some metadata of graph.
+// Attributes indicates some metadata of graph
 type Attributes struct {
 	Directed bool
+}
+
+// New will return one empty graph
+func New() *Graph {
+	g := new(Graph)
+
+	g.Vertexes = make(map[string]vertex.Vertex)
+	g.Edges = make(map[string]edge.Edge)
+
+	g.Attributes.Directed = true
+
+	return g
 }
