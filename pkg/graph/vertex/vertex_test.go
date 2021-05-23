923e396b9aec3c468ea49eabf08df9608c7e49e7
Pan Li <panli@microsoft.com>
2021-05-23 22:28:08 +0800

    Make vertex id immutable.
    * Introduce IsSame for vertex.
    
    Signed-off-by: Pan Li <panli@microsoft.com>

--- pkg/graph/vertex/vertex_test.go
+++ pkg/graph/vertex/vertex_test.go
@@ -11,7 +11,7 @@ func TestVertexNew(t *testing.T) {
 	v := New()
 
 	assert.NotNil(t, v)
-	assert.NotNil(t, v.ID)
+	assert.NotNil(t, v.id)
 
 	assert.NotNil(t, v.AdjacencyList)
 	assert.Equal(t, v.AdjacencyList.Len(), 0)
@@ -19,18 +19,17 @@ func TestVertexNew(t *testing.T) {
 	assert.Nil(t, v.Value)
 	assert.False(t, v.Status.Visitd)
 
-	assert.NotEqual(t, v.ID, New().ID)
+	assert.NotEqual(t, v.id, New().id)
 }
 
 func TestVertexInit(t *testing.T) {
 	v := New()
-	v1 := New()
 
-	v.AdjacencyList.PushBack(v1)
+	v.AdjacencyList.PushBack(New())
 	v.Value = "test-string"
 	v.Status.Visitd = true
 
-	id := v.ID
+	id := v.id
 
 	v.Init()
 
@@ -40,5 +39,14 @@ func TestVertexInit(t *testing.T) {
 	assert.Equal(t, v.AdjacencyList.Len(), 0)
 
 	assert.False(t, v.Status.Visitd)
-	assert.NotEqual(t, id, v.ID)
+	assert.Equal(t, id, v.id)
+}
+
+func TestVertexSame(t *testing.T) {
+	v := New()
+	v1 := New()
+
+	assert.True(t, v.IsSame(v))
+	assert.False(t, v.IsSame(v1))
+	assert.False(t, v.IsSame(nil))
 }
